0b742000d84a26e2bdd1fc19aa9cb5ff057490f1
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-29 23:25:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,11 +20,6 @@ def find_element_index(array, value_to_find)
   end
 end
 
-def find_element_index(array, value_to_find)
-  # Add your solution here
-  array.find_index(value_to_find)
-end
-
 def find_max_value(array)
   # Add your solution here
   max = array[0]
